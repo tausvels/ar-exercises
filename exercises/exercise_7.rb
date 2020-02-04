a3730e084f60e24157b2ee6984bf921e5206a83b
tausvels <engr.tausifkhan@gmail.com>
2020-02-03 20:39:01 -0800

    error thrown

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter Store Name!"
+store_name=gets.chomp
+
+@new_store=Store.new(name: store_name)
+@new_store.save!
+puts store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3, message: "Name must have a minimum of three characters" }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, message: "Revenue must be greater than 0" }
+  validates :annual_revenue, numericality: {only_integer: true, message: "Revenue must be an integer."}
+  validates :mens_apparel, inclusion: { in: [true, false], message: "Men or Women Store?" }
+  validates :womens_apparel, inclusion: { in: [true, false], message: "Is there a Women's Apparel department at this store?  No one knows!!" }
+
 end
